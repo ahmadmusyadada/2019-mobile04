d38ae4f34c76f0105e3612e09eb5b32e2987d769
Ahmad Musyadad Aminullah <ahmadmusyadadaminullah@gmail.com>
2019-09-16 14:11:36 +0700

    Explicit latihan

--- Intent/app/src/main/java/id/ac/polinema/intent/ExplicitIntentActivity.java
+++ Intent/app/src/main/java/id/ac/polinema/intent/ExplicitIntentActivity.java
@@ -8,11 +8,17 @@ import android.widget.EditText;
 import android.widget.TextView;
 
 public class ExplicitIntentActivity extends AppCompatActivity {
-
+    private EditText input;
+    private TextView output;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_explicit_intent);
     }
 
+    public void handleExplicitIntent(View view) {
+        input = findViewById(R.id.input_name);
+        output = findViewById(R.id.text_output);
+        output.setText("Hello " + input.getText() +", Congratulations!");
+    }
 }
