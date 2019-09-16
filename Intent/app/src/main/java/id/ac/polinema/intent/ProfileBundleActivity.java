fc5c0f7bb9010f02913c243b900a72a9ee8c6f70
Ahmad Musyadad Aminullah <ahmadmusyadadaminullah@gmail.com>
2019-09-16 15:26:20 +0700

    Bundle Activity

--- Intent/app/src/main/java/id/ac/polinema/intent/ProfileBundleActivity.java
+++ Intent/app/src/main/java/id/ac/polinema/intent/ProfileBundleActivity.java
@@ -2,9 +2,17 @@ package id.ac.polinema.intent;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class ProfileBundleActivity extends AppCompatActivity {
+    public static final String USERNAME_KEY = "username";
+    public static final String NAME_KEY = "name";
+    public static final String AGE_KEY = "age";
+    private TextView usernameText;
+    private TextView nameText;
+    private TextView ageText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -12,10 +20,21 @@ public class ProfileBundleActivity extends AppCompatActivity {
         setContentView(R.layout.activity_profile_bundle);
 
         // TODO: bind here
+        usernameText = findViewById(R.id.text_username);
+        nameText = findViewById(R.id.text_name);
+        ageText = findViewById(R.id.text_age);
 
         Bundle extras = getIntent().getExtras();
+
         if (extras != null) {
             // TODO: display value here
+            String username = extras.getString("username");
+            String name = extras.getString("name");
+            String age = String.valueOf(extras.getInt("age"));
+
+            usernameText.setText(username);
+            nameText.setText(name);
+            ageText.setText(age);
         }
     }
 }
